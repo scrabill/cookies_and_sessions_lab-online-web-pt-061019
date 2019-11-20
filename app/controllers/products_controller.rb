e465c45411d016df079c100cdd183be33da4570a
scrabill <shannon.crabill@gmail.com>
2019-11-19 21:00:52 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,12 @@
 class ProductsController < ApplicationController
   def index
-
   end
 
   def add
     # raise params
-    session[:cart] << params[:product]
+    @product = params[:product]
+    cart << @product
+    # session[:cart] << params[:product]
     # cart << params[:product]
     render :index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :products
 
   get '/', to: 'products#index'
-  post '/add', to: 'products#add'
+  post '/', to: 'products#add'
 end
